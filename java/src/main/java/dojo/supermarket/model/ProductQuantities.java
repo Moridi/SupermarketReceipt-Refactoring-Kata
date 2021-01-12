3c791cdd48ee7dd8084553c728ff99d20f60ef80
zhivar <zhivarsourati@gmail.com>
2021-01-12 09:47:09 +0330

    Adds the ProductQuantities class and moves the logic to that class

---
+++ java/src/main/java/dojo/supermarket/model/ProductQuantities.java
@@ -0,0 +1,28 @@
+package dojo.supermarket.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductQuantities {
+    private final Map<Product, Double> productQuantitiesMap = new HashMap<>();
+    private final List<ProductQuantity>  productQuantityList = new ArrayList<>();
+
+    public List<ProductQuantity> getItems() {
+        return productQuantityList;
+    }
+
+    public void add(Product product, double quantity) {
+        productQuantityList.add(new ProductQuantity(product, quantity));
+        if (productQuantitiesMap.containsKey(product)) {
+            productQuantitiesMap.put(product, productQuantitiesMap.get(product) + quantity);
+        } else {
+            productQuantitiesMap.put(product, quantity);
+        }
+    }
+
+    public double getItemQuantities(Product product) {
+        return productQuantitiesMap.get(product);
+    }
+}
