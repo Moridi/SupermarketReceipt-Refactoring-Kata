3d8654c27bf9c61d378ad6cc2d1e2a153834cc62
Mohammad Moridi <m.moridi.2009@gmail.com>
2021-01-12 17:28:39 +0330

    Add a dummy BundleOffer class

--- java/src/main/java/dojo/supermarket/model/offer/BundleOffer.java
+++ java/src/main/java/dojo/supermarket/model/offer/BundleOffer.java
@@ -1,4 +1,23 @@
 package dojo.supermarket.model.offer;
 
-public class BundleOffer {
+import dojo.supermarket.model.product.Product;
+import dojo.supermarket.model.shoppingCart.Discount;
+import dojo.supermarket.model.shoppingCart.ShoppingCart;
+import dojo.supermarket.model.supermarket.SupermarketCatalog;
+
+import java.util.Map;
+
+public class BundleOffer extends Offer {
+
+    private final Map<Product, Integer> bundleProdcuts;
+
+    public BundleOffer(SpecialOfferType offerType, Map<Product, Integer> bundleProdcuts) {
+        super(offerType);
+        this.bundleProdcuts = bundleProdcuts;
+    }
+
+    @Override
+    public Discount apply(ShoppingCart shoppingCart, SupermarketCatalog catalog) {
+        return null;
+    }
 }
--- java/src/main/java/dojo/supermarket/model/offer/Offer.java
+++ java/src/main/java/dojo/supermarket/model/offer/Offer.java
@@ -4,14 +4,12 @@ import dojo.supermarket.model.shoppingCart.Discount;
 import dojo.supermarket.model.shoppingCart.ShoppingCart;
 import dojo.supermarket.model.supermarket.SupermarketCatalog;
 
-public class Offer {
+public abstract class Offer {
     private SpecialOfferType offerType;
 
     public Offer(SpecialOfferType offerType) {
         this.offerType = offerType;
     }
 
-    public Discount apply(ShoppingCart shoppingCart, SupermarketCatalog catalog) {
-        return null;
-    }
+    public abstract Discount apply(ShoppingCart shoppingCart, SupermarketCatalog catalog);
 }
